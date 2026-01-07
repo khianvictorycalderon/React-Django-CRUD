8174d2d6b665a3050c8bfa9ed603dcc05c76f21d
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-07 13:36:20 +0800

    Added input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,9 @@ export default function App() {
         <h2 className="text-4xl font-semibold">React + Django CRUD</h2>
         <hr className="my-4" />
 
+        <span className="font-semibold text-2xl">Add Item</span>
+        <input type="text" className="bg-white" />
+
       </div>
 
     </div>
