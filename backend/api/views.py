2fe0e93670b6840748cc5b61a38a36250f8e649f
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-07 14:36:50 +0800

    Updated views (Added POST method)

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import HttpResponse
+import json
+from django.http import JsonResponse
 
 # Create your views here.
-def demo(_request):
-    return HttpResponse("API is working!")
+def demo(request):
+
+    if request.method == "POST":
+
+        # Parse JSON from body
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({
+                "message": "Invalid JSON"
+            }, status = 400)
+        
+        first_name = data.get("first_name")
+        last_name = data.get("last_name")
+        age = data.get("age")
+
+        print("Received data:")
+        print("First Name:", first_name)
+        print("Last Name:", last_name)
+        print("Age:", age)
